eda0e87a883d36d875c8e7b37405dafc180efa05
swrlly <25042344+swrlly@users.noreply.github.com>
2025-09-14 11:28:35 +0000

    data

--- datacollection/main.py
+++ datacollection/main.py
@@ -18,6 +18,7 @@ def main():
     tasks = Tasks(links, db_link, logger)
     clock = time.monotonic()
     current_day = dt.datetime.now().day
+    first_job = True
 
     while True:
 
@@ -26,9 +27,10 @@ def main():
         tasks.get_player_count(now)
         num_reviews = tasks.db_connection.count_reviews()[0]
         # get reviews every day
-        if num_reviews == 0 or dt.datetime.now().day - current_day != 0:
+        if first_job or dt.datetime.now().day - current_day != 0:
             thread = Thread(target = tasks.get_steam_reviews)
             thread.start()
+            first_job = False
             logger.info("Getting steam reviews.")
         # review data inserted after next playercount lookup
         tasks.insert_into_database()
